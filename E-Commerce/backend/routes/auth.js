677949f20ff3114fad7b57d2ada1861e2377de64
ashu vishwakarma <ashwaniashwani438@gmail.com>
2025-10-03 22:41:59 +0530

    lvl - 3 E-Commerce 2nd commit updated mongouri localhost to cluster and add some product images

--- E-Commerce/backend/routes/auth.js
+++ E-Commerce/backend/routes/auth.js
@@ -33,7 +33,7 @@ router.post('/register', async (req, res) => {
     const token = jwt.sign(
       { id: user._id, isAdmin: user.isAdmin },
       process.env.JWT_SECRET,
-      { expiresIn: '1d' }
+      { expiresIn: '7d' }
     );
 
     // Return sanitized user + token
