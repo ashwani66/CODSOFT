e21c69d85a89f8e7d78e1977125487194ec64c32
ashu vishwakarma <ashwaniashwani438@gmail.com>
2025-10-04 12:39:30 +0530

    this is commit where i have make changes in the server.js

--- E-Commerce/backend/server.js
+++ E-Commerce/backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 // ---------- Middleware ----------
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
+app.use("api/uploads", express.static("uploads"));
 
 // ---------- MongoDB Connection ----------
 mongoose.connect(process.env.MONGO_URI)
