b7d032c718a57284757683886416d312cb97d098
ashu vishwakarma <ashwaniashwani438@gmail.com>
2025-10-19 20:30:30 +0530

    set the API

--- Task-Manager/frontend/src/services/api.js
+++ Task-Manager/frontend/src/services/api.js
@@ -16,7 +16,7 @@ export function removeToken() {
 }
 
 // ------------------ Axios Instance ------------------
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://codsoft-alsb.onrender.com/api';
 const api = axios.create({ baseURL: API_BASE });
 
 // Attach token to every request if exists
